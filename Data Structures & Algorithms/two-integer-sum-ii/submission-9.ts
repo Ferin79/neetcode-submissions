d04e9cae548e4cbcb3bca6713c4cb11710dfdea6
Ferin Patel <43402168+Ferin79@users.noreply.github.com>
2026-07-10 14:28:10 -0300

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let results = [0, 0];
+        let i = 0;
+        let j = numbers.length - 1;
+
+        while (i < numbers.length && i < j) {
+            const a = numbers[i];
+            const b = numbers[j];
+
+            if (a + b === target) {
+                return [i + 1, j + 1];
+            } else if (a + b > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return results;
+    }
+}
