478d8ee162c5561cb8ffbddef523d7e51720b3e5
Ferin Patel <43402168+Ferin79@users.noreply.github.com>
2026-07-28 10:18:46 -0300

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix: number[][], target: number): boolean {
+        const len = matrix[0].length - 1;
+
+        let i = 0;
+        let j = len;
+
+        while (i < matrix.length) {
+            if (target === matrix[i][0] || target === matrix[i][j]) {
+                return true;
+            } else if (target < matrix[i][0]) {
+                return false;
+            } else if (target < matrix[i][j]) {
+                let a = 0;
+                let b = len;
+
+                while (a < b) {
+                    const mid = Math.floor((a + b) / 2);
+
+                    if (matrix[i][mid] < target) {
+                        a = mid + 1;
+                    } else if (matrix[i][mid] > target) {
+                        b = mid - 1;
+                    } else {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                i++;
+                j = len;
+            }
+        }
+
+        return false;
+    }
+}
